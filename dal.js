2bcadff6b938552197950722fb71e4d2e0976211
Brandon Aiton <brandonaiton@icloud.com>
2017-08-19 19:07:28 -0400

    added lots of nots and correctly linked all pages with auth

--- dal.js
+++ dal.js
@@ -1,24 +1,22 @@
-let userArr = require('./userArr.js');
+  let users = [
+    {id: 1, name: 'Brandon Aiton', username: 'brandon', password: 'example'},
+    {id: 2, name: 'Andrew Aiton', username: 'andrew', password: 'password'},
+    {id: 3, name: 'New Friend', username: 'guest', password: 'password'},
 
-function addUserInfo(loginId, loginPass){
-  let newLogin = {id:(userArr.length +1), username: loginId, password: loginPass};
-  userArr.push(newLogin);
-  console.log(newLogin);
+  ]
+
+function getUser(userId){
+  const foundUser = users.find(usr => Number(userId) === usr.id)
+  return foundUser                                                // when passed a number looks through to find same userid number
 }
 
-
-
-// function checkLogin(loginId, loginPass){
-//   return userArr.map(function(login){
-//     if((login.username == loginId) && (login.password == loginPass)){
-//       console.log("Success!")
-//     } else {
-//       console.log("Unable to Login check Username and Password.")
-//     }
-//   })
-// }
-
-module.export= {
-  addUserInfo: addUserInfo
-  // checkLogin:checkLogin
+function getUserByUsername (usrname) {
+  const foundUser = users.find(usr => usrname === usr.username)
+  return foundUser                                                // takes a username and looks through an array to find the username used in arguement
 }
+
+function getUsers(){
+  return users;                                                   // gives array of users when called
+}
+
+module.exports= { getUsers, getUserByUsername, getUser }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const session = require('express-session')
 const mustache = require('mustache-express')
 const express = require('express')
 const nodemon = require('nodemon')
+const bodyParser = require('body-parser')
 const dal = require('./dal.js')
 const app = express();
 
@@ -9,27 +10,64 @@ app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + "/views");
 
-app.use(express.static('public'));
-// app.use(session({
-//   genid: function(req) {
-//     return genuuid() // use UUIDs for session IDs
-//   },
-//   secret: 'keyboard cat'
-// }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(express.static('./public'));
 
-app.get('/login', function(req, res){
-  res.render("login")
+app.use(
+  session({
+    secret: 'this is cool',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: null }
+  })
+)
+
+app.use(function (req, res, next) {
+  if (req.session.usr) {                                                    //truthy statement that says there is a current user
+    req.isAuthenticated = true
+  } else {
+    req.isAuthenticated = false
+  }
+  console.log(req.session, 'req.session')
+  next()
+})
+// *********************-Routes-***********************
+app.get('/', function(req, res){                                          // at the url '/'
+                                                                          // console.log(req.session)  this will show the cookies from this page
+  res.render("home", {isAuthenticated: req.isAuthenticated})
 })
 
-app.post('/login', function(req,res){
-  dal.addUserInfo(req.params.username, req.params.password);  //wtf username and password *************
-  // dal.checkLogin(req.body.username, req.body.password)
-  res.redirect('/');
+app.get('/admin', function (req, res) {                                   //at the url '/admin'
+  if (req.isAuthenticated) {
+  const users = dal.getUsers()                                            //give the array of users when called
+    res.render('admin', { users: users, loggedUsr: req.session.usr })     //create admin.mustach if authenticated
+  } else {
+  res.redirect('/')                                                       //redirect to '/'
+  }
 })
 
-app.get('/', function(req, res){
-  res.render("home")
+app.get('/login', function(req, res){                                     // at the url '/login'
+  res.render("login")                                                     // create page login.mustache
+})
+
+app.get('/loggedout', function(req, res){                                 // at the url '/loggedout/'
+  req.session.destroy()                                                   //end logged in session when visited
+  res.render("loggedout")                                                 // show the loggedout mustache file
+})
+
+
+
+app.post('/login', function (req, res) {                                 //when the post from action '/login' come through
+  const foundUsr = dal.getUserByUsername(req.body.username)              //passes username to check for matching username information
+  if (req.body.password === foundUsr.password) {
+    console.log(req.session.usr);                        //checks for matching passwords
+    req.session.usr = { name: foundUsr.name }                            // I dunno why but we reasign the session usrname here??? seems odd.
+    res.redirect('/')                                                    //send to the url '/admin'
+  } else {
+    res.send('womp womp')                                                //show text 'womp womp' if pasword doesnt match the called username.
+  }
 })
 
 
